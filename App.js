6aab58c57a221e794e46720959db0cc04aef75ee
wahyuwibowo <wahyuwibowo@mdd.co.id>
2020-11-09 18:45:14 +0700

    add screen login

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ScreenLogin from './src/screens/LoginScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScreenLogin />
   );
 }
 
