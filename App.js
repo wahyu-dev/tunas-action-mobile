f07b73ca7f8ae14e1058ecf13b3a75f8f1c9e67f
wahyuwibowo <wahyuwibowo@mdd.co.id>
2020-11-09 21:03:53 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ScreenLogin from './src/screens/LoginScreen';
+import ScreenLogin from './src/pages/LoginScreen';
 
 export default function App() {
   return (
     <ScreenLogin />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
